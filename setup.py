dd29957bac2b2fee6cc02da6fded07b3172fb617
Rahul Arulkumaran <rahuldravid313@gmail.com>
2017-12-12 00:16:10 +0530

    added lyrics feature as an entry point and module

--- setup.py
+++ setup.py
@@ -6,17 +6,20 @@ setup(
 	py_modules=[
 		'fbcli',
 		'cricbuzz',
+		'lyrics',
 	],
 	install_requires=[
 		'click',
 		'bs4',
 		'BeautifulSoup',
 		'mechanize',
+		'requests',
 	],
 	entry_points={
 		'console_scripts':[
 		'fbcli=fbcli:cli',
 		'cricbuzz=cricbuzz:cli',
+		'lyrics=lyrics:cli',
 		]
 	},
 )
