6363ed66c40b4b03711d49c73b50a21845337934
Rahul Arulkumaran <rahuldravid313@gmail.com>
2017-12-15 00:24:19 +0530

    Update searching.py

--- searching.py
+++ searching.py
@@ -1,5 +1,4 @@
 import mechanize
-from bs4 import BeautifulSoup
 import google
 import click
 import wikipedia
@@ -19,13 +18,13 @@ The page results are displayed on the command line itself.
 @click.option('--wiki',is_flag=True,help='Gives you the wikipedia page for the topic')
 
 def cli(srch,wiki):
-	browser = mechanize.Browser()
-	browser.set_handle_robots(False)	#Allows everything to be written
-	cookies = mechanize.CookieJar()
-	browser.set_cookiejar(cookies)
-	browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
-	browser.set_handle_refresh(False)	#Sometimes hangs without this	
 	if(srch):
+		browser = mechanize.Browser()
+		browser.set_handle_robots(False)	#Allows everything to be written
+		cookies = mechanize.CookieJar()
+		browser.set_cookiejar(cookies)
+		browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
+		browser.set_handle_refresh(False)	#Sometimes hangs without this
 		query = raw_input("Enter the topic you want to search about: ") 
 		for link in google.search(query, tld="com", num=10, stop=1, pause=2):	#To get the first few links related to the topic.
 			print link	#Click on the displayed link to open page on browser
