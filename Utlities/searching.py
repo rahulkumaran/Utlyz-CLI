039d6181c8059035488594c360c05f22ca4156f5
Rahul Arulkumaran <rahuldravid313@gmail.com>
2017-12-18 17:14:14 +0530

    Created folder for all utlities

---
+++ Utlities/searching.py
@@ -0,0 +1,28 @@
+import mechanize
+from bs4 import BeautifulSoup
+import google
+import click
+import wikipedia
+
+@click.command()
+
+@click.option('--google',is_flag=True,help='Searches the results related to the topic you enter')
+
+@click.option('--wiki',is_flag=True,help='Gives you the wikipedia page for the topic')
+
+def cli(google,wiki):
+	browser = mechanize.Browser()
+	browser.set_handle_robots(False)	#Allows everything to be written
+	cookies = mechanize.CookieJar()
+	browser.set_cookiejar(cookies)
+	browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
+	browser.set_handle_refresh(False)	#Sometimes hangs without this	
+	if(google):
+		query = raw_input("Enter the topic you want to search about: ")
+		for link in google.search(query, tld="co.in", num=10, stop=1, pause=2):
+			print link
+	if(wiki):
+		wiki_topic = raw_input('Enter the topic you want to read about: ')
+		result = wikipedia.page(title=wiki_topic,auto_suggest=True,redirect=True, preload=False)
+		wiki_content = result.content
+		print wiki_content
---
+++ Utlities/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+	name='utlyz',
+	version=1.0,
+	py_modules=[
+		'fbcli',
+		'cricbuzz',
+		'lyrics',
+		'searching',
+	],
+	install_requires=[
+		'click',
+		'bs4',
+		'BeautifulSoup',
+		'mechanize',
+		'requests',
+		'google',
+		'wikipedia',
+	],
+	entry_points={
+		'console_scripts':[
+		'fbcli=fbcli:cli',
+		'cricbuzz=cricbuzz:cli',
+		'lyrics=lyrics:cli',
+		'searching=searching:cli',
+		]
+	},
+)
