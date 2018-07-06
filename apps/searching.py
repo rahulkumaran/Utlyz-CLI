d502141ace1a317171763c2ed58a6fa7a7311fbb
rahulkumaran <rahuldravid313@gmail.com>
2018-07-07 02:48:39 +0530

    updated google library

--- apps/searching.py
+++ apps/searching.py
@@ -1,22 +1,22 @@
 import mechanize
-from google import search
+from googlesearch import search
 import click
 import wikipedia
 
 @click.command()
 
-@click.option('--google',is_flag=True,help='Searches the results related to the topic you enter')
+@click.option('--goog',is_flag=True,help='Searches the results related to the topic you enter')
 
 @click.option('--wiki',is_flag=True,help='Gives you the wikipedia page for the topic')
 
-def cli(google,wiki):
+def cli(goog,wiki):
 	browser = mechanize.Browser()
 	browser.set_handle_robots(False)	#Allows everything to be written
 	cookies = mechanize.CookieJar()
 	browser.set_cookiejar(cookies)
 	browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.41 Safari/534.7')]
 	browser.set_handle_refresh(False)	#Sometimes hangs without this
-	if(google):
+	if(goog):
 		query = raw_input("Enter the topic you want to search about: ")
 		for link in search(query, tld="co.in", num=10, stop=1, pause=2):
 			print link
